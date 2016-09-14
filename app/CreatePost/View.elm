3a41dab4cd7b7e7655aea7d18d3d401432d222c6
JScearcy <jacobscearcy@gmail.com>
2016-09-14 14:58:07 -0500

    Add fonts for code block styling

--- app/CreatePost/View.elm
+++ app/CreatePost/View.elm
@@ -1,7 +1,7 @@
 module CreatePost.View exposing (view)
 
 import Html exposing (Html, div, textarea)
-import Html.Attributes exposing (rows, class)
+import Html.Attributes exposing (rows, class, id)
 import Html.Events exposing (onInput)
 import Markdown exposing (Options, defaultOptions, toHtmlWith)
 import CreatePost.Update exposing (Msg(..))
@@ -16,6 +16,6 @@ options =
 view : Model -> Html Msg
 view { input } =
     div []
-        [ textarea [ rows 15, class "post-input", onInput TextChange ] []
+        [ textarea [ rows 15, class "post-input", id "post-area",  onInput TextChange ] []
         , toHtmlWith options [ class "preview" ] input
         ]
