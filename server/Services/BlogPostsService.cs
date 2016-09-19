204d9d832f81a03d5a079ac77878d1fac8f7a9bd
JScearcy <jacobscearcy@gmail.com>
2016-09-19 16:22:46 -0500

    removing unused usings

--- server/Services/BlogPostsService.cs
+++ server/Services/BlogPostsService.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using WebApplication.Models;
 using WebApplication.Data;
+using WebApplication.Models;
 
 namespace WebApplication.Services
 {
